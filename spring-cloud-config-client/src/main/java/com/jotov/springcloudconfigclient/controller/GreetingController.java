bab64f393eeab24ae3d24685e5b60a1ba9c4f449
Владимир Йотов <jotovvj@cifra-k.ru>
2021-01-22 15:34:51 +0700

    Preparing controller

---
+++ spring-cloud-config-client/src/main/java/com/jotov/springcloudconfigclient/controller/GreetingController.java
@@ -0,0 +1,23 @@
+package com.jotov.springcloudconfigclient.controller;
+
+
+import com.jotov.springcloudconfigclient.config.DbSettings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class GreetingController {
+    @Value("${my.greeting: default value}")
+    private String greetingMessage;
+
+    @Autowired
+    private DbSettings dbSettings;
+
+    @GetMapping("greetings")
+    public String greeting() {
+        return "my.greeting: " + greetingMessage + " db.connection: " + dbSettings.getConnection();
+    }
+}
