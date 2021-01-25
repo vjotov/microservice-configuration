7459a7b8c472cf7a5aa4c169ea715e586242cdd7
Владимир Йотов <jotovvj@cifra-k.ru>
2021-01-25 11:36:19 +0700

    update

--- spring-cloud-config-client/src/main/java/com/jotov/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/jotov/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -1,9 +1,11 @@
 package com.jotov.springcloudconfigclient;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableAutoConfiguration
 public class SpringCloudConfigClientApplication {
 
 	public static void main(String[] args) {
--- spring-cloud-config-client/src/main/java/com/jotov/springcloudconfigclient/controller/GreetingController.java
+++ spring-cloud-config-client/src/main/java/com/jotov/springcloudconfigclient/controller/GreetingController.java
@@ -4,13 +4,16 @@ package com.jotov.springcloudconfigclient.controller;
 import com.jotov.springcloudconfigclient.config.DbSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RefreshScope
 public class GreetingController {
     @Value("${my.greeting: default value}")
+
     private String greetingMessage;
 
     @Autowired
